69341257e8234f3d8a2e4a3c4b8e7390f7b2a2d5
carlosm <carlosalcides190304@gmail.com>
2025-03-02 00:11:05 -0400

    Termine con la lectura de archivo de forma sincrona, viendo los detalles generales del archivo

--- Clase1/modulos_nativos/modulo_file/3.fs-stats.js
+++ Clase1/modulos_nativos/modulo_file/3.fs-stats.js
@@ -1,12 +1,14 @@
 const fs = require('node:fs') // a partir de v16 de Node.js, se recomienda poner node: 
 
+//forma sincrono, secuencial...
+
 //información sobre el archivo
 const stats = fs.statSync('archivo.txt')
 
 //Con esto podemos recuperar información de un archivo 
 console.log(
-    stats.isFile(),
-    stats.isDirectory(),
-    stats.isSymbolicLink(),
-    stats.size
+    stats.isFile(), // si es un archivo 
+    stats.isDirectory(), //si es un directorio 
+    stats.isSymbolicLink(), // si es un enlace simbolico 
+    stats.size //tanali en bytes 
 )
