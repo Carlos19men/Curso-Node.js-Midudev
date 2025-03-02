db1fac1a7eaf0a27cf364aec94e8fd7c6a5288f2
carlosm <carlosalcides190304@gmail.com>
2025-03-02 00:12:07 -0400

    creé el segundo archivo visualizar el manejo de la ejecución sincrona viendo como los archivos se leen de forma secuencial

--- Clase1/modulos_nativos/modulo_file/4.fs-readFile.js
+++ Clase1/modulos_nativos/modulo_file/4.fs-readFile.js
@@ -6,3 +6,11 @@ const text = fs.readFileSync('./archivo.txt','utf-8')
 //la codificacion es opcional, pero es util para poder interpretar la información
 
 console.log(text)
+
+
+
+console.log('Leyendo el segundo archivo ')
+
+const secondText = fs.readFileSync('./archivo2.txt','utf-8')
+
+console.log(secondText)
