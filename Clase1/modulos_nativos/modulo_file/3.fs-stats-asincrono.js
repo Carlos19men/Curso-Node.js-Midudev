a33707bffe657d6fab0fbc1011eecf540ad6d295
carlosm <carlosalcides190304@gmail.com>
2025-03-03 14:43:28 -0400

    Coloque el código del ejemplo de abriendo archivos de forma asincrona

---
+++ Clase1/modulos_nativos/modulo_file/3.fs-stats-asincrono.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs')
+console.log('leyendo el archivo de forma asincrona') 
+
+fs.readFile('archivo.txt', 'utf-8', (err,text) => {
+    console.log('archivo leido') 
+    console.log(text)
+})
+//el call back en este caso es el cuerpo de la función 
+
+for(let i = 0; i< 100; i++){
+    console.log('->'+i)
+}
+//que se van a ejecutar en segundo plano (err,text)
