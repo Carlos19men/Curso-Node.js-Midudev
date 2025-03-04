f3ec6e872cfe3716c64538cb2305cb773af0a240
carlosm <carlosalcides190304@gmail.com>
2025-03-04 13:10:57 -0400

    Agregué el process para visualizar como podemos trabajar con el entorno del sistema operativo y del script

---
+++ Clase1/modulos_nativos/modulo_file/9.process.js
@@ -0,0 +1,19 @@
+// argumentos de entrada
+/*
+console.log(process.argv)
+
+//podemos controlar el proceso y su salida 
+//console.log(process.exit(1))
+
+//podemos controlar eventos del proceso
+process.on('exit',(()=>{
+    //limpiar recursos
+}))
+
+
+*/
+//current working directory , me devuelve la ruta donde se esta ejecutando el script
+console.log(process.cwd())
+
+//platform, me devuelve el sistema operativo
+console.log(process.env.VAR1)
