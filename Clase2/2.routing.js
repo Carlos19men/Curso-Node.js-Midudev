aa2ac4f660e5e39c2924285ce7e945d18beca3d4
carlosm <carlosalcides190304@gmail.com>
2025-03-09 01:22:17 -0400

    Configuramos las funciones para crear una api usando el json de pokemon

--- Clase2/2.routing.js
+++ Clase2/2.routing.js
@@ -1,7 +1,25 @@
 const http = require('hode:http')
+const dittoJson = require('./pokemon/ditto.json')
 
 const processRequest = (req,res) => {
+    //de cada reques vamos a sacar el metodo y la url
+    
+    const {method, url } = req
 
+    switch (method){
+        case 'GET': 
+            switch(url){
+                case '/pokemon/ditto':
+                    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+                    return res.end(JSON.stringify(dittoJson))
+                default:
+                    res.statusCode = 404
+                    res.setHeader('content-Type', 'text/html; charset=utf-8')   
+                    return res.end('<h1> 404 </h1>')
+            }
+        case 'POST': 
+            
+    }
 }
 
 const sever = http.createServer(processRequest)
