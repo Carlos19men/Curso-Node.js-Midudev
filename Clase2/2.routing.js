62ac48dbfcf3583ab14431ffa43e4db6c0ce5a8b
carlosm <carlosalcides190304@gmail.com>
2025-03-10 16:49:16 -0400

    forma nativa de hacer una API

--- Clase2/2.routing.js
+++ Clase2/2.routing.js
@@ -4,6 +4,7 @@ const dittoJson = require('./pokemon/ditto.json')
 const processRequest = (req,res) => {
     //de cada reques vamos a sacar el metodo y la url
     
+    //obtenermos el metodo y la url de la request 
     const {method, url } = req
 
     switch (method){
@@ -15,16 +16,38 @@ const processRequest = (req,res) => {
                 default:
                     res.statusCode = 404
                     res.setHeader('content-Type', 'text/html; charset=utf-8')   
-                    return res.end('<h1> 404 </h1>')
+                    return res.end('<h1> 404 NOT FOUND </h1>')
             }
         case 'POST': 
             switch (url) {
                 case '/pokemon': {
                     let body = ''
-                    break; 
-                    //?????
+                    
+                    //escuchar el evento data 
+                    req.on('data', chunck => {
+                        body += chunck.toString()
+                    })
+
+                    //finalizado el evento 
+                    req.on('end', () => {
+                        const data = JSON.parse(body)
+
+                        //llamar a una base de datos para guardar la info
+                        res.writeHead(201, {'Content-Type': 'application/json; charset=utf-8'})
+                       
+                        data.timestap = Date.now()
+                        res.end(JSON.stringify(data))
+                    })
+
+                    break
                 }
-                   
+
+                //en caso de algun error 
+                default: 
+                    res.statusCode = 404
+                    res.setHeader('Content-Type', 'text/plain; charsert=utf-8')
+                    return res.end('<h1> 404 not found...</h1>')
+                    
             }
             
     }
