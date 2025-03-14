ddf8704749e34436a97ec225a976a6cf205a73f2
carlosm <carlosalcides190304@gmail.com>
2025-03-13 23:33:18 -0400

    hice otra función usando el parsel() para hacer que los campos del recursos sean opcionales

--- Clase3/schemas/movies.js
+++ Clase3/schemas/movies.js
@@ -14,14 +14,14 @@ const movieSchema = zod.object({
     year: zod.number().int().min(1900).max(2022),
     director: zod.string(),
     duration: zod.number().int().positive(),
-    rate: zod.number().min(0).max(10),
+    rate: zod.number().min(0).max(10).default(5),
     poster: zod.string().url({
         message: 'poster must be a valid URL '
     }),
     /*En esta caso el enum especifamos las opciones posibles 
     que se pueden colocar
     */
-    genre: zod.array(zod.enum(['Action','Adventure','Comedy','Drama','Fantasy','Horror']),{
+    genre: zod.array(zod.enum(['Action','Crime','Adventure','Comedy','Drama','Fantasy','Horror']),{
         required_error: 'Movie genre is required',
         invalid_type_error: 'Movie genre must be an array of enum'
     })
@@ -35,6 +35,18 @@ function validateMovie (object) {
     return movieSchema.safeParse(object)
 }
 
+/*
+    partial en un metodo en typescritp con el que 
+    hacemos que todos los campos de esta estructura 
+    sean opcionales, en este caso estamos diciendo 
+    que para el esquema de validación los campos van 
+    a ser opcionales, no pasa nada ni no estan 
+*/
+function validatePartialMovie(input){
+    return movieSchema.partial().safeParse(input)
+}
+
 module.exports = {
-    validateMovie
+    validateMovie,
+    validatePartialMovie
 }
