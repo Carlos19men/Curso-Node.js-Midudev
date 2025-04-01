bc33ac9c761ab8f5e1b42a2eb3459cbc2eb4ceb0
carlos19men <carlosalcides190304@gmail.com>
2025-03-31 20:42:53 -0400

    prueba de token

--- Clase4/utils.js
+++ Clase4/utils.js
@@ -2,3 +2,4 @@ import { createRequire } from 'node:module'
 export const require = createRequire(import.meta.url) 
 
 export const readJSON = (path) => require(path) 
+//puse un comentario KKAKAKAKAKAKAKAKA
