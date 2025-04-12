acf2d2fc833925e0619b4568b92d7e3aee77b710
carlos mendez <carlosalcides190304@gmail.com>
2025-04-12 09:46:41 -0400

    I included the function to create to server instances

--- Clase5/app.js
+++ Clase5/app.js
@@ -1,28 +1,32 @@
 import express, { json } from 'express'
-import { readJSON, require } from './utils.js'
-import { moviesRouter } from './routes/movies.js'
 import { corsMidleware } from './middlewares/cors.js'
+import { createMovieRouter } from './routes/movies.js'
 
-const app = express()
-app.use(json())
-app.use(corsMidleware())
-app.disable('x-powered-by')
+export const createApp = ({movieModel}) =>{
+    const app = express()
+    app.use(json())
+    app.use(corsMidleware())
+    app.disable('x-powered-by')
 
-
-app.get('/', (req, res) => {
-    res.json({message: 'Hello World'})
-})
-
-//con esto ya estamos aplanando la aplicación 
-//agrupando la ruta 
-app.use('/movies', moviesRouter)
+    //con esto ya estamos aplanando la aplicación 
+    //agrupando la ruta 
+    app.use('/movies', createMovieRouter({movieModel}))
 
 
-const PORT = process.env.PORT ?? 1234
+    const PORT = process.env.PORT ?? 1234
 
-//Escuchando el puerto 
-app.listen(PORT, () => {
-    console.log(`Server is running on port http://localhost:${PORT}`)
-})
+    //Escuchando el puerto 
+    app.listen(PORT, () => {
+        console.log(`Server is running on port http://localhost:${PORT}`)
+    })
+
+    return app
+}
+
+
+
+
+
+
 
 
